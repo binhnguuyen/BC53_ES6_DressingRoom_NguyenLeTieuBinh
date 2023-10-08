ce3567259842f50dfa9047d58f786f541421648b
binhnguyen <leechiupin@gmail.com>
2023-10-09 00:13:06 +0900

    Thêm hàm thử đồ (đang lỗi)

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,8 @@
 const URL = "https://65191bfd818c4e98ac600e5f.mockapi.io/listphukien";
 const nhomURL = "https://65191bfd818c4e98ac600e5f.mockapi.io/nhomphukien";
 
-import PhuKien from "../models/ChoseItem.js"
-import List from "../models/ListChosen.js"
+// import PhuKien from "../models/ChoseItem.js"
+// import List from "../models/ListChosen.js"
 
 /**
  * @param {*} getEle 
@@ -16,42 +16,29 @@ const getEle = (selector) => {
 
 
 /**
- * @param {*} getPhuKien 
- * Chức năng: mock API
- * Tham số: 
- * Chú ý: 
- */
-window.thuDo = (id) => {
-    console.log(id);
-}
-
-
-/**
  * @param {*} renderPhuKien 
  * Chức năng: render phụ kiện ra giao diện
  * Tham số: 
  * Chú ý: 
  */
-const renderPhuKien = (list, id) => {
+const renderPhuKien = (list, tabID) => {
     let htmlContent = "";
 
     list.forEach((value) => {
-        console.log('value: ', value);
         htmlContent += `
             <div class="my-4 col-12 col-lg-4 col-sm-6">
-                <div class="card animate__animated  animate__zoomIn">
-                    <img src="${value.imgSrc_jpg}   "class="card-img-top" alt=""width="200" >
-                    <div>
+                <div class="card animate__animated animate__zoomIn">
+                    <img src="${value.imgSrc_jpg}" class="card-img-top" alt=""width="200" >
+                    <div class="text-primary text-center">
                         <h5>${value.name}</h5>
                     </div>
-                    <button type="button" onclick="thuDo(${value.id})" class="btn btn-primary">Thử  đồ</button>
+                    <button id="btn-${value.id}" type="button" onclick="thuDo(${value.id})" class="btn btn-primary">Thử  đồ</button>
                 </div>
             </div>
-        
         `
     })
 
-    getEle(`#${id}`).innerHTML = htmlContent;
+    getEle(`#${tabID}`).innerHTML = htmlContent;
 }
 
 
@@ -77,12 +64,12 @@ const getPhuKien = () => {
     let typehairstyle = "hairstyle";
     let typebackground = "background";   
 
-    const promise = axios({
-        method: "GET",
-        url: URL,
-    })
-
-    promise
+    // const promise = axios({
+    //     method: "GET",
+    //     url: URL,
+    // })
+    
+    getProductList()
         //get data thành công
         .then((result) => {
             let product = result.data;            
@@ -142,16 +129,12 @@ getPhuKien();
  */
 const renderNhomPhuKien = (list) => {
     let htmlContent = "";
-    // console.log('list: ', list);
-    // console.log('list[0]: ', list[0]);
-    // console.log('list[0].tabName: ', list[0].tabName);
 
     list.forEach((value) => {
-        // console.log('value: ', value);
         htmlContent += `
         <li class="nav-item" role="presentation">
             <button
-              class="nav-link"
+              class="nav-link text-justify"
               id="pills-${value.tabName}-tab"
               data-toggle="pill"
               data-target="#${value.tabName}"
@@ -201,11 +184,66 @@ getNhomPhuKien();
 /**
  * @param {*} thuDo 
  * Chức năng: hàm thử đồ
- * Tham số: 
+ * Tham số: id
  * Chú ý: 
  */
+window.thuDo = (idPhuKien) => {
 
+    // console.log('idPhuKien: ', idPhuKien);
 
+    editProductByID(idPhuKien)
+    .then(function (res) {
+        // gán cái get đc cho sp
+        var sp = res.data;
+        // console.log('sp: ', sp);
+
+      })
+      .catch(function (err) {
+      //   offLoading();
+        console.log("err", err);
+      });
+}
+
+//     const promise = axios({
+//         method: "GET",
+//         url: `${URL}/${idPhuKien}`,
+//     })
+
+//     promise
+//         //get data thành công
+//         .then((result) => {
+//             var phuKien = result.data;
+//             console.log('phuKien: ', phuKien);
+//             console.log(result.data);
+//         })
+//         // get data thất bại
+//         .catch((err) => {
+//             console.log("err: ", err);
+//         })
+//         // luôn luôn chạy dù thành công, thất bại
+//         .finally(() => {
+            
+//         })
+// }
+
+// window.thuDo = (id) => {
+
+//     editProductByID(id)
+
+//         //get data thành công
+//         .then((result) => {
+//             console.log(result.data);
+//         })
+//         // get data thất bại
+//         .catch((err) => {
+//             console.log("err: ", err);
+//         })
+//         // luôn luôn chạy dù thành công, thất bại
+//         .finally(() => {
+            
+//         })
+// }
+// thuDo();
 
 
 
